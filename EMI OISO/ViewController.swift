392e1fc003f9c3f6bddd18d5f3ba3231464bb966
emioiso <jijjjiijiijijiji@gmail.com>
2024-05-25 11:07:29 +0900

    Initial Commit

---
+++ EMI OISO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EMI OISO
+//
+//  Created by emi oiso on 2024/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
